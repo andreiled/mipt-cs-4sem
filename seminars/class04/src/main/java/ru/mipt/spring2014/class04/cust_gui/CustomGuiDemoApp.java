14fb0b385a9d74b12e48ece82b01aa1fe5668330
andreiled <led@frtk.ru>
2014-03-03 14:40:28 +0400

    Custom GUI Demo

---
+++ seminars/class04/src/main/java/ru/mipt/spring2014/class04/cust_gui/CustomGuiDemoApp.java
@@ -0,0 +1,39 @@
+/**
+ * CustomGuiDemoApp.java
+ * Created on Feb 25, 2014
+ */
+package ru.mipt.spring2014.class04.cust_gui;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+
+public class CustomGuiDemoApp
+{
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main (String args[])
+	{
+		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+		try
+		{
+			UIManager.setLookAndFeel (UIManager.getSystemLookAndFeelClassName ());
+		} catch (Exception ex)
+		{
+			Logger.getLogger (GraphDemoWindow.class.getName ()).log (Level.SEVERE, "Couldn't set Look&Feel", ex);
+		}
+		//</editor-fold>
+
+		/*
+		 * Create and display the form
+		 */
+		java.awt.EventQueue.invokeLater (new Runnable ()
+		{
+			public void run ()
+			{
+				new GraphDemoWindow ().setVisible (true);
+			}
+		});
+	}
+}
---
+++ seminars/class04/src/main/java/ru/mipt/spring2014/class04/cust_gui/ImageMover.java
@@ -0,0 +1,94 @@
+/**
+ * ImagePositioner.java
+ * Created On Apr 24, 2013
+ * @author Andrei
+ */
+package ru.mipt.spring2014.class04.cust_gui;
+
+import java.awt.Point;
+import javax.swing.JComponent;
+
+public class ImageMover extends Thread
+{
+	private final JComponent component;
+
+	private int width = 0, height = 0;
+	private Point position = new Point ();
+	private int vx = 0, vy = 0;
+
+	public ImageMover (JComponent component)
+	{
+		this.component = component;
+	}
+
+	public void setVelocity (int vx, int vy)
+	{
+		this.vx = vx;
+		this.vy = vy;
+	}
+
+	public synchronized void resize (int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+	}
+
+	public Point getCurrentPosition ()
+	{
+		return new Point (position);
+	}
+
+	private Point calculateNewPosition ()
+	{
+		Point newPos = new Point (position);
+		newPos.x += vx;
+		newPos.y += vy;
+		if (newPos.x < 0)
+		{
+			newPos.x = -newPos.x;
+			vx = -vx;
+		}
+		if (newPos.y < 0)
+		{
+			newPos.y = -newPos.y;
+			vy = -vy;
+		}
+		if (newPos.x >= width)
+		{
+			newPos.x = 2 * width - newPos.x;
+			vx = -vx;
+		}
+		if (newPos.y >= height)
+		{
+			newPos.y = 2 * height - newPos.y;
+			vy = -vy;
+		}
+		return newPos;
+	}
+
+	public synchronized void stepOver ()
+	{
+		if (width == 0 || height == 0 || Math.abs (vx) >= width || Math.abs (vy) >= height)
+		{
+			return;
+		}
+
+		position = calculateNewPosition ();
+		component.repaint ();
+	}
+
+	@Override
+	public void run ()
+	{
+		while (!Thread.interrupted ())
+		{
+			stepOver ();
+			try{
+				Thread.sleep (20L);
+			} catch (InterruptedException e)
+			{
+				break;
+			}
+		}
+	}
+}
